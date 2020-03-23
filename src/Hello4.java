e55eee8d3420832f71b1ba66c9e3bf7200f9ba61
nPaloukosQA <62283168+nPaloukosQA@users.noreply.github.com>
2020-03-23 22:28:49 +0200

    Question 4

---
+++ src/Hello4.java
@@ -0,0 +1,14 @@
+public class Hello4 {
+
+    static String planB() {
+        return "Hello World";
+    }
+
+    public static void main (String[] args) {
+
+        System.out.println(planB());
+
+    }
+
+
+}
